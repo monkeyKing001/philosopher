b11783a3d88e33e4276a343b3000ba8883a502e2
Dong woo Kwak <dokwak@c8r6s9.42seoul.kr>
2022-10-03 15:32:56 +0900

    fixing delay

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: dokwak <dokwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/30 18:58:43 by dokwak            #+#    #+#             */
-/*   Updated: 2022/10/01 22:52:54 by dokwak           ###   ########.fr       */
+/*   Updated: 2022/10/03 15:31:18 by dokwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,13 @@ void	print_state(t_desk *desk, int phil_idx, int STATUS)
 	t_philosopher	*phil;
 
 	phil = &(desk -> phils[phil_idx]);
-	if (STATUS == SLEEPING && check_die_desk(desk, phil_idx, CHECK) == FALSE)
+	if (STATUS == DIED \
+			&& check_die_desk(desk, phil_idx, CHECK) == FALSE)
+		printf("%lld %d died\n", \
+				get_time_interval(phil -> birth_ms, \
+				get_time_ms()), phil_idx + 1);
+	else if (STATUS == SLEEPING \
+			&& check_die_desk(desk, phil_idx, CHECK) == FALSE)
 		printf("%lld %d is sleeping\n", get_timestamp(phil), phil_idx + 1);
 	else if (STATUS == THINKING \
 			&& check_die_desk(desk, phil_idx, CHECK) == FALSE)
@@ -58,8 +64,4 @@ void	print_state(t_desk *desk, int phil_idx, int STATUS)
 		printf("%lld %d has taken a fork\n", get_timestamp(phil), phil_idx + 1);
 	else if (STATUS == EATING && check_die_desk(desk, phil_idx, CHECK) == FALSE)
 		printf("%lld %d is eating\n", get_timestamp(phil), phil_idx + 1);
-	else if (STATUS == DIED && check_die_desk(desk, phil_idx, CHECK) == FALSE)
-		printf("%lld %d died\n", \
-				get_time_interval(phil -> birth_ms, \
-				get_time_ms()), phil_idx + 1);
 }
